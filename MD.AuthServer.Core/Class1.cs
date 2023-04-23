ccd34e82952579fefce24ce75b7d44ffc97eb112
MDNET-Developer <qarabagli.murad@mail.ru>
2023-04-23 18:19:02 +0400

    JWT AuthServer

---
+++ MD.AuthServer.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MD.AuthServer.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ MD.AuthServer.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MD.AuthServer.Data
+{
+    public class Class1
+    {
+    }
+}
