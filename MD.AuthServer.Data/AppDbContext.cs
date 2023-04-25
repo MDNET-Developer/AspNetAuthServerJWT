7ce35f31dd6919fdd13ac6aad4a87db84ff7d6e9
MDNET-Developer <qarabagli.murad@mail.ru>
2023-04-25 07:57:08 +0400

    DbContext Confuguration

--- MD.AuthServer.Data/AppDbContext.cs
+++ MD.AuthServer.Data/AppDbContext.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using MD.AuthServer.Core.Model;
+using MD.AuthServer.Data.Confuguration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MD.AuthServer.Data
 {
-    public class AppDbContext:IdentityDbContext
+    public class AppDbContext:IdentityDbContext<UserApp,IdentityRole,string>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductConfugration());
+            builder.ApplyConfiguration(new UserRefreshTokenConfugration());
+        }
     }
 }
---
+++ MD.AuthServer.Data/Confuguration/UserRefreshTokenConfugration.cs
@@ -0,0 +1,21 @@
+﻿using MD.AuthServer.Core.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD.AuthServer.Data.Confuguration
+{
+    public class UserRefreshTokenConfugration : IEntityTypeConfiguration<UserRefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<UserRefreshToken> builder)
+        {
+            builder.HasKey(x => x.UserId);
+            builder.Property(x=>x.Code).IsRequired().HasMaxLength(250);   
+
+        }
+    }
+}
