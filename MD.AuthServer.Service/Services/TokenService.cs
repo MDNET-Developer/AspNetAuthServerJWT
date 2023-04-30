bd38e22bdf4a90ebcbb38b73162740f98781b53a
MDNET-Developer <qarabagli.murad@mail.ru>
2023-04-30 21:20:52 +0400

    Update Claim

--- MD.AuthServer.Service/Services/TokenService.cs
+++ MD.AuthServer.Service/Services/TokenService.cs
@@ -7,7 +7,9 @@ using Microsoft.Extensions.Options;
 using SharedLiblary.Confuguration;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,6 +29,21 @@ namespace MD.AuthServer.Service.Services
         private string CreateRefreshToken()
         {
             return Guid.NewGuid().ToString();
+        }
+
+        private IEnumerable<Claim> GetClaim(UserApp user,List<String> audences)
+        {
+            var userlist = new List<Claim>()
+            {
+            new Claim(ClaimTypes.NameIdentifier,user.Id),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(ClaimTypes.Name,user.UserName),
+            //Her bir tokenin ID-sidir bir nov
+            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            userlist.AddRange(audences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+            return userlist;
         }
 
         public TokenDto CreateToken(UserApp userApp)
